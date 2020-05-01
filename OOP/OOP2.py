87647f868929a653f31937d39e23358c5748b55f
mark banford <markbanford@gmail.com>
2020-05-01 14:53:23 +0100

    class

---
+++ OOP/OOP2.py
@@ -0,0 +1,38 @@
+# Class Variables are shared amoungst all instances such as annual raise
+
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = self.first + '.' + self.last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+emp1 = Employee('Mark', 'Banford', 50000)
+
+# lets see the namespace of emp1. We will see there is no raise amount
+# since we are inheriting from the parent class
+
+print(emp1.__dict__)
+
+# if we want to give employee 1 5% and not 4%, we can do the following
+
+emp1.raise_amount = 1.10
+
+# now we have added this attribute in the emp1 namespace only
+print(emp1.__dict__)
+emp1.apply_raise()
+print(emp1.pay)
+
+emp2 = Employee('Dan', 'Baker', 50000)
+print(emp2.__dict__)
+emp2.apply_raise()
+print(emp2.pay)
